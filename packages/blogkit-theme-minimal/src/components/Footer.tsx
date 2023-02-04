53b8cbe39a11bb1834a0faaca9427e9687213954
lingPoint <44696270+lingPoint@users.noreply.github.com>
2023-02-04 17:44:42 +0800

    Update Footer.tsx

--- packages/blogkit-theme-minimal/src/components/Footer.tsx
+++ packages/blogkit-theme-minimal/src/components/Footer.tsx
@@ -2,13 +2,8 @@ export function Footer() {
   return (
     <div className="text-sm">
       Powered by{' '}
-      <a target="_blank" href="https://github.com/2nthony/blogkit">
-        blogkit
-      </a>
-      . Inspired by{' '}
-      <a target="_blank" href="https://github.com/djyde/sairin">
-        Sairin
-      </a>
+      <a target="_blank" href="https://github.com/lingPoint">
+        Zerox
       .
     </div>
   )
